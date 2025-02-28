7b98e4d0224f4bf65b25d25aff12c1279e9d2e72
RazvanNeon <craciun.razvan.neon@gmail.com>
2025-02-28 13:51:07 +0200

    z

--- test_https_socket.py
+++ test_https_socket.py
@@ -1,28 +1,45 @@
-﻿import socket
+﻿import os
+import socket
+import threading
+from flask import Flask
 
-# Configurare server
-HOST = '0.0.0.0'  # Ascultă pe toate interfețele de rețea
-PORT = 52345      # Portul pe care rulează serverul
+# Configurare aplicație Flask
+app = Flask(__name__)
 
-# Crearea unui socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen(1)  # Ascultă o conexiune
+# Configurare server socket
+HOST = '0.0.0.0'
+PORT = 12345
 
-print(f"Serverul este pornit și ascultă pe portul {PORT}...")
+def start_socket_server():
+    """Serverul socket care ascultă pe un port specific."""
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(5)
+    print(f"Serverul socket este pornit și ascultă pe {HOST}:{PORT}...")
+    
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Conexiune acceptată de la: {client_address}")
+        
+        # Primirea și procesarea datelor de la client
+        data = client_socket.recv(1024).decode()
+        print(f"Mesaj primit: {data}")
+        
+        # Răspuns către client
+        response = f"Salut! Mesajul tău a fost primit: {data}"
+        client_socket.sendall(response.encode())
+        
+        client_socket.close()
 
-while True:
-    # Acceptarea conexiunii de la un client
-    client_socket, client_address = server_socket.accept()
-    print(f"Conexiune acceptată de la: {client_address}")
+# Pornirea serverului socket într-un fir de execuție separat
+socket_thread = threading.Thread(target=start_socket_server, daemon=True)
+socket_thread.start()
 
-    # Primirea datelor de la client
-    data = client_socket.recv(1024).decode()
-    print(f"Mesaj primit: {data}")
+# Endpoint HTTP simplu
+@app.route('/')
+def home():
+    return "Server HTTP și socket este în funcțiune!"
 
-    # Procesarea și trimiterea unui răspuns înapoi
-    response = f"Serverul a primit: {data}"
-    client_socket.sendall(response.encode())
-
-    # Închiderea conexiunii cu clientul
-    client_socket.close()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Portul pentru Flask
+    app.run(host='0.0.0.0', port=port)
