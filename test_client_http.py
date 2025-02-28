9d09670d7a4ed81781aaaf7df907179e6072c44d
RazvanNeon <craciun.razvan.neon@gmail.com>
2025-02-28 12:42:15 +0200

    z

---
+++ test_client_http.py
@@ -0,0 +1,22 @@
+﻿import socket
+
+# Configurare client
+HOST = 'localhost'  # Adresa serverului (în acest caz, local)
+PORT = 12345        # Portul serverului
+
+# Crearea unui socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Conectarea la server
+client_socket.connect((HOST, PORT))
+
+# Trimiterea unui mesaj către server
+message = "Salutare, server!"
+client_socket.sendall(message.encode())
+
+# Primirea răspunsului de la server
+response = client_socket.recv(1024).decode()
+print(f"Răspuns de la server: {response}")
+
+# Închiderea conexiunii
+client_socket.close()
---
+++ test_https_socket/test_https_socket.py
@@ -0,0 +1,28 @@
+﻿import socket
+
+# Configurare server
+HOST = '0.0.0.0'  # Ascultă pe toate interfețele de rețea
+PORT = 12345      # Portul pe care rulează serverul
+
+# Crearea unui socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)  # Ascultă o conexiune
+
+print(f"Serverul este pornit și ascultă pe portul {PORT}...")
+
+while True:
+    # Acceptarea conexiunii de la un client
+    client_socket, client_address = server_socket.accept()
+    print(f"Conexiune acceptată de la: {client_address}")
+
+    # Primirea datelor de la client
+    data = client_socket.recv(1024).decode()
+    print(f"Mesaj primit: {data}")
+
+    # Procesarea și trimiterea unui răspuns înapoi
+    response = f"Serverul a primit: {data}"
+    client_socket.sendall(response.encode())
+
+    # Închiderea conexiunii cu clientul
+    client_socket.close()
