c0e0112f6cdc0968553cafa7cccd3a0c8fa541de
RazvanNeon <craciun.razvan.neon@gmail.com>
2025-03-17 18:07:43 +0200

    Update test_https_socket.py

--- test_https_socket.py
+++ test_https_socket.py
@@ -72,7 +72,9 @@ def home():
 @app.route('/status', methods=['GET'])
 def status():
     # Endpoint suplimentar pentru a obține starea conexiunii
+    client_ip = request.headers.get("X-Forwarded-For", request.remote_addr)
     return jsonify({
+        'client_ip': client_ip,
         'active_clients': active_clients,
         'memo_msg': memo_msg
     })
